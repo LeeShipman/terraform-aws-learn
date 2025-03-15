eaf28d5f79be243c455db49d2d5c6397ede2fb48
Lee Shipman <lshipman@hotmail.co.uk>
2025-03-15 01:38:42 +0000

    Updated provider version and added VPC module

--- main.tf
+++ main.tf
@@ -2,36 +2,34 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "my-vpc"
+  cidr = var.vpc_cidr_block
 
-resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = "${var.env_prefix}-vpc"
+  azs            = [var.avail_zone]
+  public_subnets = [var.subnet_cidr_block]
+
+  public_subnet_tags = {
+    name = "${var.env_prefix}-subnet-1"
   }
-}
-
-module "myapp-subnet" {
-  source                 = "./modules/subnet"
-  subnet_cidr_block      = var.subnet_cidr_block
-  avail_zone             = var.avail_zone
-  env_prefix             = var.env_prefix
-  vpc_id                 = aws_vpc.myapp-vpc.id
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
+  tags = {
+    name = "${var.env_prefix}-vpc"
+  }
 }
 
 module "my-app-webserver" {
   source              = "./modules/webserver"
   my_ip               = var.my_ip
-  vpc_id              = aws_vpc.myapp-vpc.id
+  vpc_id              = module.vpc.vpc_id
   env_prefix          = var.env_prefix
   public_key_location = var.public_key_location
   instance_type       = var.instance_type
   avail_zone          = var.avail_zone
   image_name          = var.image_name
   ami_architecture    = var.ami_architecture
-  subnet_id           = module.myapp-subnet.subnet.id
+  subnet_id           = module.vpc.public_subnets[0]
 
 }
 
